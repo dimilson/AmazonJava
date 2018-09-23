07bdb32557361f49831e3ee11af1c6dd3fbab160
David Augusto <davidaugusto.ifal@gmail.com>
2018-09-23 16:53:24 -0300

    criação do repositorio de carrinho

---
+++ demoAmazon/src/main/java/com/example/demoAmazon/repositorios/CarrinhoRepository.java
@@ -0,0 +1,9 @@
+package com.example.demoAmazon.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demoAmazon.model.Carrinho;
+
+public interface CarrinhoRepository extends JpaRepository<Carrinho, Integer>{
+
+}
