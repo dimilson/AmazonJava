6d1ce579766dfce114d5bd35e4f956d388cf6811
Edmilson <edmilsonoliveira.ifal@gmail.com>
2018-09-25 08:47:29 -0300

    Adição de um novo metodo no repositorio do cliente

--- demoAmazon/src/main/java/com/example/demoAmazon/repositorios/ClienteRepository.java
+++ demoAmazon/src/main/java/com/example/demoAmazon/repositorios/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.example.demoAmazon.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.example.demoAmazon.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	List <Cliente> findByNome(String nome);
 
 }
