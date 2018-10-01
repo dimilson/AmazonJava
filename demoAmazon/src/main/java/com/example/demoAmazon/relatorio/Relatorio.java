aeda4026e4be32fafa60474bb09cd37d3aa3b37e
Aristoteles <aripbarros@gmail.com>
2018-10-01 11:04:33 -0300

    Criação de novos métodos nas classes mais alguns testes

--- demoAmazon/src/main/java/com/example/demoAmazon/relatorio/Relatorio.java
+++ demoAmazon/src/main/java/com/example/demoAmazon/relatorio/Relatorio.java
@@ -1,5 +1,8 @@
 package com.example.demoAmazon.relatorio;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.example.demoAmazon.model.Produto;
@@ -8,8 +11,10 @@ public class Relatorio {
 
 	private Double maiorPreco = Double.NEGATIVE_INFINITY;
 	private Double menorPreco = Double.POSITIVE_INFINITY;
+	
+	private List<Produto> produtosMaisCaros;
 
-	public void getRelatorioPreco(List<Produto> produtos) {
+	public void gerarRelatorioPrecos(List<Produto> produtos) {
 
 		for (Produto produto : produtos) {
 			if (produto.getPreco() > maiorPreco) {
@@ -18,8 +23,21 @@ public class Relatorio {
 			if (produto.getPreco() < menorPreco) {
 				menorPreco = produto.getPreco();
 			}
-		}
+		}	
+	  calcularProdutosMaisCaros(produtos);
+	}
 
+	private void calcularProdutosMaisCaros(List<Produto> produtos) {
+		produtosMaisCaros = new ArrayList<>(produtos);
+		Collections.sort(produtosMaisCaros, new Comparator<Produto>() {
+			public int compare(Produto p1, Produto p2) {
+				if(p1.getPreco() < p2.getPreco()) return 1;
+				if(p1.getPreco() > p2.getPreco()) return -1;
+					return 0;
+			}
+		});
+		produtosMaisCaros = produtosMaisCaros.subList(0, 3);
+		
 	}
 
 	public Double getMaiorPreco() {
@@ -30,4 +48,9 @@ public class Relatorio {
 		return menorPreco;
 	}
 
+	public List<Produto> getProdutosMaisCaros() {
+		return produtosMaisCaros;
+	}
+
+	
 }
--- demoAmazon/src/test/java/relatorio/RelatorioTest.java
+++ demoAmazon/src/test/java/relatorio/RelatorioTest.java
@@ -25,7 +25,7 @@ public class RelatorioTest {
 		produtos.add(dvd);
 
 		Relatorio relatorio = new Relatorio();
-		relatorio.getRelatorioPreco(produtos);
+		relatorio.gerarRelatorioPrecos(produtos);
 		
 		Double maiorPrecoEsperado = 400.0;
 		Double menorPrecoEsperado = 250.0;
@@ -53,7 +53,7 @@ public class RelatorioTest {
 		
 
 		Relatorio relatorio = new Relatorio();
-		relatorio.getRelatorioPreco(produtos);
+		relatorio.gerarRelatorioPrecos(produtos);
 		
 		Double maiorPrecoEsperado = 400.0;
 		Double menorPrecoEsperado = 250.0;
@@ -79,7 +79,7 @@ public class RelatorioTest {
 		
 
 		Relatorio relatorio = new Relatorio();
-		relatorio.getRelatorioPreco(produtos);
+		relatorio.gerarRelatorioPrecos(produtos);
 		
 		Double maiorPrecoEsperado  = 400.0;
 		Double menorPrecoEsperado = 250.0;
@@ -102,7 +102,7 @@ public class RelatorioTest {
 		produtos.add(tv);		
 
 		Relatorio relatorio = new Relatorio();
-		relatorio.getRelatorioPreco(produtos);
+		relatorio.gerarRelatorioPrecos(produtos);
 		
 		Double maiorPrecoEsperado  = 300.0;
 		Double menorPrecoEsperado = 300.0;
@@ -112,5 +112,31 @@ public class RelatorioTest {
 
 	}
 	
+	@Test
+	public void deveEncontrarOsTresProdutosMaisCaros() {
+		
+		Produto caneta = new Produto("Caneta Bik", 1.50);
+		Produto celular = new Produto("Moto G500", 250.0);
+		Produto tv = new Produto("Samsung HD", 300.0);
+		Produto dvd = new Produto("CCE DVD", 400.0);
+
+		List<Produto> produtos = new ArrayList<>();
+		produtos.add(caneta);
+		produtos.add(celular);
+		produtos.add(tv);
+		produtos.add(dvd);
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerarRelatorioPrecos(produtos);
+		
+		List<Produto> top3ProdutosMaisCaros = relatorio.getProdutosMaisCaros();
+	
+		assertEquals(3, top3ProdutosMaisCaros.size());
+		assertEquals(new Double (400.0), top3ProdutosMaisCaros.get(0).getPreco());
+		assertEquals(new Double (300.0), top3ProdutosMaisCaros.get(1).getPreco());
+		assertEquals(new Double (250.0), top3ProdutosMaisCaros.get(2).getPreco());
+
+
+	}
 
 }
