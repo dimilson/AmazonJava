85e03f75df04e6c595bb4c7f5ee335f629c74204
David Augusto <davidaugusto.ifal@gmail.com>
2018-10-08 18:48:49 -0300

    Implementação da classe controller de cliente

--- demoAmazon/src/main/java/com/example/demoAmazon/controllers/ClienteController.java
+++ demoAmazon/src/main/java/com/example/demoAmazon/controllers/ClienteController.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.util.List;
 
 import com.example.demoAmazon.model.Cliente;
 import com.example.demoAmazon.repositorios.ClienteRepository;
+import com.pw2.nozama.model.Usuario;
 
 @Controller
 @RequestMapping("/cliente")
@@ -20,11 +23,33 @@ public class ClienteController {
 	
 	@GetMapping(value = "/list")
 	public String listCliente (ModelMap model) {
+		
 		List<Cliente> clientes = clienteRepository.findAll();
 		model.addAttribute("clienteList", clientes);
 		
 		
 		return "cliente/list";
+	}
+	
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.GET)
+	public String cadastrar() {
+
+		return "cliente/cadastro";
+	}
+	
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+	public String cadastrar(Cliente user) {
+		clienteRepository.save(user);
+
+		return "redirect:/cliente/list";
+	}
+	
+	@RequestMapping("/deletar")
+	public String deletar(Integer id) {
+		
+		clienteRepository.deleteById(id);
+		
+		return "redirect:/cliente/list";
 	}
 	
 	
