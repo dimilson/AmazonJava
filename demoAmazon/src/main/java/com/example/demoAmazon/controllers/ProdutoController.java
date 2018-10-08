86ffa7fd49475d17070dd72a9b743c7c6ecf448b
David Augusto <davidaugusto.ifal@gmail.com>
2018-10-08 18:54:42 -0300

    Implementação da classe controller de produto

--- demoAmazon/src/main/java/com/example/demoAmazon/controllers/ProdutoController.java
+++ demoAmazon/src/main/java/com/example/demoAmazon/controllers/ProdutoController.java
@@ -1,9 +1,15 @@
 package com.example.demoAmazon.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demoAmazon.model.Produto;
 import com.example.demoAmazon.repositorios.ProdutoRepository;
 
 @Controller
@@ -12,4 +18,35 @@ public class ProdutoController {
 
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@GetMapping(value = "list")
+	public String listarProduto(ModelMap model) {
+		List<Produto> produtos = produtoRepository.findAll();
+
+		model.addAttribute("listaProdutos", produtos);
+
+		return "produto/list";
+	}
+	
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.GET)
+	public String form() {
+
+		return "produto/cadastrar";
+	}
+	
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+	public String form(Produto produto) {
+
+		produtoRepository.save(produto);
+
+		return "redirect:/produto/list";
+	}
+	
+	@RequestMapping("/deletar")
+	public String deletarProduto(Integer id) {
+
+		produtoRepository.deleteById(id);
+
+		return "redirect:/produto/list";
+	}
 }
